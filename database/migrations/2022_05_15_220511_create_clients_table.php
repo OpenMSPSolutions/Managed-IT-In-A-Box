45bc52a4a44e7472e877d6e6c881a368028e5b83
pistone10 <dev@pistone.xyz>
2022-05-15 18:45:09 -0600

    Started adding client functions

---
+++ database/migrations/2022_05_15_220511_create_clients_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('team_id');
+            $table->string('name');
+            $table->string('phone_number')->nullable();
+            $table->foreignId('primary_user_id')->nullable();
+            $table->foreignId('account_manager_user_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+};
